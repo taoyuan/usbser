2b97a2c403555662f60b8f5127367bcfbc3af8b5
taoyuan <torworx@gmail.com>
2015-12-07 19:06:43 +0800

    0.1.0

--- examples/watch.js
+++ examples/watch.js
@@ -1,18 +1,16 @@
-/**
- * Created by taoyuan on 15/12/7.
- */
-
 "use strict";
 
-var watch = require('watch');
-watch.createMonitor('/dev', function (monitor) {
-  monitor.on("created", function (f, stat) {
-    console.log('created', f);
-  });
-  monitor.on("changed", function (f, curr, prev) {
-    // Handle file changes
-  });
-  monitor.on("removed", function (f, stat) {
+var fs = require('fs');
+var usbser = require('../');
+
+usbser.watch('/dev')
+  .on('ready', function () {
+    console.log('ready');
+  })
+  .on('created', function (f, stat) {
+    var realpath = fs.realpathSync(f);
+    console.log('created', f, '->', realpath);
+  })
+  .on('removed', function (f, stat) {
     console.log('removed', f);
   });
-});
